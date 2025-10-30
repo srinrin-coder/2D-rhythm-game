3126a4b661c6bd06069e1853616fb3e0eaf0fdc2
srinrin-coder <rin.water318@gmail.com>
2025-10-30 13:42:00 +0900

    タイムラインツール導入

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -9,6 +9,7 @@ public class Background : MonoBehaviour
     private float backgroundWidth;
     private Vector3 startPosition;
 
+
     void Start()
     {
 
@@ -41,6 +42,7 @@ public class Background : MonoBehaviour
         Debug.Log("【背景】リスポーン通知を受け取りました。位置を " + startPosition + " にリセットします。", this.gameObject);
         transform.position = startPosition;
     }
+
     void Update()
     {
         Move();
@@ -48,22 +50,6 @@ public class Background : MonoBehaviour
 
     void Move()
     {
-        // 1. 移動 (変更なし)
         transform.Translate(Vector3.right * scrollSpeed * Time.deltaTime);
-
-        // 2. ループ判定とテレポート (修正箇所)
-
-        // 判定条件:
-        // 背景オブジェクトのX座標が、現在のカメラの中心よりも
-        // 「背景1枚分の幅」だけ左側に出たらテレポートする。
-        // backgroundWidth / 2 ではなく backgroundWidth にしているのは、
-        // 画面の幅ではなく、隣り合う背景オブジェクトの幅でループさせるためです。
-        if (transform.position.x < Camera.main.transform.position.x - backgroundWidth)
-        {
-            // テレポート先:
-            // 現在の位置から、背景2枚分（backgroundWidth * 2）だけ右に瞬間移動させる。
-            // これにより、もう1枚の背景オブジェクトの真後ろにテレポートし、シームレスに繋がります。
-            transform.position = new Vector2(transform.position.x + backgroundWidth * 2, transform.position.y);
-        }
     }
 }
